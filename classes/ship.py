69b1eef8d6e420e4fcd49ac14a338f59b508e440
v1nte <ivinte99@gmail>
2020-07-29 11:21:14 -0400

    adding background and ship

---
+++ classes/ship.py
@@ -0,0 +1,40 @@
+import pygame
+pygame.init()
+
+class Ship():
+    def __init__(self, color, ship_type, Screen_width, Screen_height):
+        '''doc string'''
+        self.x = 2 
+        self.y = 0
+        self.velocity = 10
+        self.hp = 3
+        self.color = color
+        self.ship_type = ship_type
+        self.img_path = "src/img/Spaceships/"+ship_type+"/Spaceship_"+ship_type+"_"+color+".png"
+        self.img = (pygame.image.load(self.img_path))
+        self.img = pygame.transform.scale(self.img, (100,100))
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+        self.hitbox = (self.x+self.width//2, self.x-self.width//2, self.y+self.height//2, self.y-self.height//2)
+
+    def draw(self, screen):
+        '''Draw where the ship is located'''
+        if self.hp > 0:
+            screen.blit(self.img, (self.x, self.y))
+
+        #   self.hitbox = (self.x+self.width//2, self.x-self.width//2, self.y+self.height//2, self.y-self.height//2)
+
+    def update(self, move_to, left, right, up, down, Screen_width, Screen_height):
+        '''This funciton implement the movement of the character/ship'''
+        if move_to[left] and self.x  > self.velocity:
+            self.x -= self.velocity
+
+        if move_to[right] and self.x + self.width + self.velocity < Screen_width:
+            self.x += self.velocity
+
+        if move_to[up] and self.y > self.velocity:
+            self.y -= self.velocity
+
+        if move_to[down] and self.y + self.height + self.velocity < Screen_height:
+            self.y += self.velocity
+
--- main.py
+++ main.py
@@ -1,1 +1,49 @@
 import pygame
+from classes.ship import Ship 
+
+pygame.init()
+
+#Screen Settings
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 900 
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Working on it...")
+Background = pygame.image.load('src/img/bg.png')
+Background = pygame.transform.scale(Background, (SCREEN_WIDTH,SCREEN_HEIGHT))
+
+#Keys Shortcuts
+K_LEFT = pygame.K_LEFT
+K_RIGHT = pygame.K_RIGHT
+K_UP = pygame.K_UP
+K_DOWN = pygame.K_DOWN
+
+clock = pygame.time.Clock()
+
+def update_and_draw():
+    '''update and draw, duh'''
+    SCREEN.blit(Background, (0,0))
+    m_ship.draw(SCREEN)
+    pygame.display.update()
+
+main_loop = True
+while main_loop:
+
+    m_ship = Ship("RED", "01", SCREEN_WIDTH, SCREEN_HEIGHT )
+#    imagen_fondo = pygame.image.load('img/bg.jpg')
+    
+    is_playing = True 
+    while is_playing:
+        for event  in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+
+        clock.tick(60)
+
+        KEYS = pygame.key.get_pressed()
+        m_ship.update(KEYS, K_LEFT, K_RIGHT, K_UP, K_DOWN, SCREEN_WIDTH, SCREEN_HEIGHT)
+
+        update_and_draw()
+
+pygame.quit()
+
+ 
