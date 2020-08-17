63ec738353449af69011135b6f4492a4736b7a8e
v1nte <ivinte99@gmail>
2020-08-17 09:02:39 -0400

    Adding a 'menu' and a Ship selector

--- classes/ship.py
+++ classes/ship.py
@@ -2,30 +2,62 @@ import pygame
 from classes.bullet import Bullet
 
 class Ship():
-    def __init__(self, color, ship_type, Screen, Screen_width, Screen_height):
-        self.x = 300
-        self.y = 800
+    def __init__(self, color, ship_type, Screen, Screen_width, Screen_height, x=300, y=800):
+        self.x = x 
+        self.y = y  
         self.velocity = 10
         self.acc = 0.1
         self.hp = 3
+        self.scale = 1/4.5
         self.color = color
         self.ship_type = ship_type
         self.img_path = "src/img/spaceships/"+ship_type+"/Spaceship_0"+ship_type+"_"+color+".png"
         self.img = (pygame.image.load(self.img_path))
-        self.width = self.img.get_width()/4.5
-        self.height = self.img.get_height()/4.5
+        self.width = self.img.get_width()*self.scale
+        self.height = self.img.get_height()*self.scale
         self.img = pygame.transform.scale(self.img, (int(self.width),int(self.height))) 
         self.hitbox = [self.x, self.y, self.width, self.height]
         self.bullets = []
 
     def get_bullets(self):
-        """Get Bullets"""
         return self.bullets
+
+    def get_hb_as_rect(self):
+        '''doc string'''
+        return pygame.Rect(self.hitbox)
+    
     def get_hp(self):
-        '''return HP'''
         return self.hp
 
+    def get_scale(self):
+        return self.scale
+    
+    def set_pos(self, x, y):
+        '''set pos of ship'''
+        self.x = x
+        self.y = y
+
+    def set_type(self,s_type, scale):
+        '''set the type of ship'''
+        self.img_path = "src/img/spaceships/"+s_type+"/Spaceship_0"+s_type+"_"+self.color+".png"
+        self.img = (pygame.image.load(self.img_path))
+        self.resize(scale)
+
+    def set_color(self, clr, scale):
+        '''set the color of ship'''
+        self.img_path = "src/img/spaceships/"+self.ship_type+"/Spaceship_0"+self.ship_type+"_"+clr+".png"
+        self.img = (pygame.image.load(self.img_path))
+        self.resize(scale)
+
+
+    def resize(self, n):
+        '''resize ship'''
+        self.width = self.img.get_width()*n
+        self.height = self.img.get_height()*n
+        self.img = pygame.transform.scale(self.img, (int(self.width),int(self.height))) 
+
     def shoot(self, KEYS, K_space, Screen, S_height):
+
         """Create the bullets and append it"""
     
         if KEYS[K_space]:
@@ -33,13 +65,16 @@ class Ship():
             self.bullets.append(B)
 
 
+    def draw_hb(self, Screen):
+        pygame.draw.rect(Screen, (255,0,0),  self.hitbox, 2)
+
     def draw(self, Screen, S_width, S_height):
         """Draw where the ship is located, and the bullets"""
 
         if self.hp > 0:
             Screen.blit(self.img, (self.x, self.y))
             #Just while coding - HITBOX
-            #pygame.draw.rect(Screen, (255,0,0),  self.hitbox, 2)
+            #draw_hb()
 
         for Bs in self.bullets: 
             Bs.update()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 from classes.ship import Ship 
 from classes.enemy import Enemy
 
@@ -22,6 +23,8 @@ K_UP = pygame.K_UP
 K_DOWN = pygame.K_DOWN
 K_SPACE = pygame.K_SPACE
 
+click = False
+
 clock = pygame.time.Clock()
 
 def KEYS():
@@ -33,6 +36,7 @@ def event_quit():
     for event  in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
+    pygame.display.update()
 
 def main_update():
     """Update logical things"""
@@ -65,14 +69,136 @@ def main_draw():
     rect.draw(SCREEN)
     pygame.display.update()
 
+def select_ship():
+    """Select type and color of the Ship"""
+    global SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT
+    global m_ship
+
+    test_ships = []
+    preview_ship = m_ship
+
+    rect_blue = pygame.Rect(80, 500, 50,50)
+    rect_green = pygame.Rect(80*2, 500, 50,50)
+    rect_n_blue = pygame.Rect(80*3, 500, 50,50)
+    rect_orange = pygame.Rect(80*4, 500, 50,50)
+    rect_purple = pygame.Rect(80*5, 500, 50,50)
+    rect_red = pygame.Rect(80*6, 500, 50,50)
+    rect_yellow = pygame.Rect(80*7, 500, 50,50)
+
+    preview_ship.set_pos(250,300)
+    preview_ship.resize(2)
+    for i in range(1,7):
+       test_ships.append(Ship("RED", str(i), SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT,  90*i, 600))
+    
+    click2 = False
+    selecting = True
+    while selecting:
+        mx, my = pygame.mouse.get_pos()
+        SCREEN.blit(Background, (0,0))
+        preview_ship.draw(SCREEN, SCREEN_HEIGHT, SCREEN_HEIGHT)
+
+        i = 0
+        for ships in test_ships:
+            ships.draw(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT)
+            if ships.get_hb_as_rect().collidepoint((mx,my)) and click2:
+                m_ship.set_type(str(i+1),m_ship.get_scale())
+                preview_ship.set_type(str(i+1), preview_ship.get_scale()*2)
+            i += 1
+
+
+
+
+        pygame.draw.rect(SCREEN, (0, 0, 255), rect_blue)
+        pygame.draw.rect(SCREEN, (0, 255, 0), rect_green)
+        pygame.draw.rect(SCREEN, (35, 35, 142), rect_n_blue)
+        pygame.draw.rect(SCREEN, (255, 165, 0), rect_orange)
+        pygame.draw.rect(SCREEN, (255, 0, 255), rect_purple)
+        pygame.draw.rect(SCREEN, (255, 0, 0), rect_red)
+        pygame.draw.rect(SCREEN, (255, 255, 0), rect_yellow)
+
+        #TEDIOUS PART!!!!!!!!!!!!!!!!!!!!!!!!
+        if rect_blue.collidepoint((mx,my)) and click2:
+            m_ship.set_color("BLUE" , m_ship.get_scale())
+            preview_ship.set_color("BLUE", preview_ship.get_scale()*2) 
+
+        if rect_green.collidepoint((mx,my)) and click2:
+            m_ship.set_color("GREEN" , m_ship.get_scale())
+            preview_ship.set_color("GREEN", preview_ship.get_scale()*2) 
+        
+        if rect_n_blue.collidepoint((mx,my)) and click2:
+            m_ship.set_color("NAVY BLUE" , m_ship.get_scale())
+            preview_ship.set_color("NAVY BLUE", preview_ship.get_scale()*2) 
+
+        if rect_orange.collidepoint((mx,my)) and click2:
+            m_ship.set_color("ORANGE" , m_ship.get_scale())
+            preview_ship.set_color("ORANGE", preview_ship.get_scale()*2) 
+
+        if rect_purple.collidepoint((mx,my)) and click2:
+            m_ship.set_color("PURPLE" , m_ship.get_scale())
+            preview_ship.set_color("PURPLE", preview_ship.get_scale()*2) 
+
+        if rect_red.collidepoint((mx,my))  and click2:
+            m_ship.set_color("RED" , m_ship.get_scale())
+            preview_ship.set_color("RED", preview_ship.get_scale()*2) 
+        
+        if rect_yellow.collidepoint((mx,my)) and click2:
+            m_ship.set_color("YELLOW" , m_ship.get_scale())
+            preview_ship.set_color("YELLOW", preview_ship.get_scale()*2) 
+
+        click2 = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                quit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click2 = True
+
+        pygame.display.update()
+
+def main_menu():
+    global click, menu_loop
+    SCREEN.blit(Background, (0,0))
+    mx, my = pygame.mouse.get_pos()
+
+    play_button = pygame.Rect(50, 100, 200, 50)
+    select_ship_button = pygame.Rect(50, 200, 200, 50)
+    
+    if play_button.collidepoint((mx,my)) and click:
+        menu_loop = False
+    if select_ship_button.collidepoint((mx,my)) and click:
+        select_ship()
+
+    pygame.draw.rect(SCREEN, (255, 0, 0), play_button)
+    pygame.draw.rect(SCREEN, (255, 0, 0), select_ship_button)
+
+    play_text = text.render("JUGAR!",1,(255,153,153))
+    SCREEN.blit(play_text, (50,100))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            quit()
+        if event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                click = True
+
+
 """
 MAIN LOOP - GAME LOOP
 
 """
 main_loop = True
 while main_loop:
-    m_ship = Ship("PURPLE", "5", SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT )
+    m_ship = Ship("NAVY BLUE", "1", SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT )
     rect = Enemy(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT) 
+
+    """
+    MENU LOOP
+    """
+    menu_loop = True
+    while menu_loop:
+        main_menu()
+        clock.tick(FPS)
+        event_quit()
 
 
     """
